b73d900de76b0b78cf57315435b9cb24a1048b23
Enoch Phetteplace <curiousaps@gmail.com>
2025-05-20 13:24:58 -0500

    Add sliders

--- app/src/main/java/com/curiousapps/pickacolor/ui/presentation/ColorPickScreen.kt
+++ app/src/main/java/com/curiousapps/pickacolor/ui/presentation/ColorPickScreen.kt
@@ -9,14 +9,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.sourceInformationMarkerStart
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -29,8 +27,6 @@ import androidx.compose.ui.unit.sp
 fun ColorPickScreen(
     viewModel: ColorPickViewModel = ColorPickViewModel()
 ){
-    //fuck you
-
     val state by viewModel.state.collectAsState(ColorPickViewModel.ColorState())
 
     val red = rememberSaveable{ mutableIntStateOf(state.red)}
