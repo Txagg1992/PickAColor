403fee1a7ea3f25f540281ff04e1906fbd748c95
Enoch Phetteplace <curiousaps@gmail.com>
2025-05-20 13:42:08 -0500

    Add Color code box

--- app/src/main/java/com/curiousapps/pickacolor/ui/presentation/ColorPickScreen.kt
+++ app/src/main/java/com/curiousapps/pickacolor/ui/presentation/ColorPickScreen.kt
@@ -1,11 +1,14 @@
 package com.curiousapps.pickacolor.ui.presentation
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
@@ -20,6 +23,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -85,6 +89,21 @@ fun ColorPickScreen(
                 viewModel.setBlue(it)
             },
             color = Color.Blue
+        )
+
+        Box(
+            modifier = Modifier
+                .size(150.dp)
+                .background(Color(
+                    red = red.intValue,
+                    green =  green.intValue,
+                    blue = blue.intValue
+                ))
+        )
+        Text(
+            text = hexcode,
+            fontSize = 20.sp,
+            textAlign = TextAlign.Center
         )
     }
 }
